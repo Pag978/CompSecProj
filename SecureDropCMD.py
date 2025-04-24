d9325fbc414d701a27e5bd94dd9da1a698232089
BenHoule-LPS <benjhoule@hotmail.com>
2025-04-24 19:03:06 -0400

    Major Changes - File Encryption & Salting
    - Added AES encryption for sensitive data
      - Files with sensitive data (user/contacts) are no longer stored in plaintext JSON format, instead the entire JSON object is encrypted for storage and decrypted for use.
    
    - Added salting to hashing algo, not very effective atm. Working more on that in the experimental branch.
    - Generalized load/store methods to clean up SecureDrop class
    - Added max login attempts
      - Exits shell & logs timestamp in `report.log` when reached

--- SecureDropCMD.py
+++ SecureDropCMD.py
@@ -1,6 +1,17 @@
-import cmd, sys, os, json, hashlib
+import cmd, sys, os, json, datetime, random
+from getpass import getpass
+from hashlib import blake2b
 from hmac import compare_digest
+from Crypto.Cipher import AES
+from Crypto.Random import get_random_bytes
 
+# TODO: Add integrity checks for contact info (Compare hashes from before encryption and after decryption?)
+# - Main question: What would be a good solution for storing these hashes? (original data needs to be accessible)
+
+SALT_SIZE = 16
+KEY_SIZE = 32
+NONCE_SIZE = 16
+TAG_SIZE = 16
 
 class SecureDrop(cmd.Cmd):
     intro = "Welcome to SecureDrop.\nType 'help' or ? to list commands.\n"
@@ -9,8 +20,6 @@ class SecureDrop(cmd.Cmd):
     contacts = {}
 
     # ----- SecureDrop Commands -----
-    # TODO: Add security for contact info (Confidentiality + Integrity)
-    # -Require password to make changes to contacts?
     def do_add(self, arg):
         """Add a new contact"""
         email = input("Enter contact's email address: ")
@@ -18,7 +27,7 @@ class SecureDrop(cmd.Cmd):
             print("Contact already exists. Updating information.")
         name = input("Enter contact's full name: ")
         self.contacts[email] = {"full_name": name}
-        self.save_contacts()
+        encrypt_and_store(self.contacts, "contacts.enc")
         print("Contact added successfully!")
         return False
 
@@ -36,15 +45,14 @@ class SecureDrop(cmd.Cmd):
         """Initiate file transfer with a contact"""
         return False
 
-    # For some reason the cmdloop just stopped exiting when this was called,
+    # For some reason the cmdloop stopped exiting when this was called,
     # so I decided to take matters into my own hands and hit it the sys.exit().
-    # -Keeping 'return True' because that is theoretically how this is supposed
-    #   to exit.
+    # -Keeping 'return True' because that is theoretically how this is supposed to exit.
     def do_exit(self, arg):
         """Exits the SecureDrop shell"""
-        print("Exiting SecureDrop")
         self.users = {}
         self.contacts = {}
+        print("Exiting SecureDrop")
         sys.exit()
         return True
 
@@ -52,29 +60,24 @@ class SecureDrop(cmd.Cmd):
     def preloop(self):
         """Check if user is registered. Initialize first time setup if not,
         continue with user login otherwise"""
-        self.load_users()
+        self.users = load_and_decrypt("users.enc")
         if len(self.users) == 0:
             self.first_time_setup()
         else:
             self.user_login()
-            self.load_contacts()
+            self.contacts = load_and_decrypt("contacts")
 
     def precmd(self, line):
         return line.lower()
 
     # ----- Custom Methods -----
-    # Do we want these methods as part of the class or as a separate module?
-    # Pros: Cleaner/Smaller class definition
-    # Cons: Lose easy access to class data
     def first_time_setup(self):
         """Request to register a new user, exit shell if denied."""
-        # If key exists but users.json does not, users.json was likely deleted.
-        # -Delete key so new one is generated during password creation.
-        # -Should maybe do the same with contacts?
-        # -Might want to move this somewhere else in case users.json was deleted
-        #   accidentally and could be recovered.
-        if os.path.exists("key"):
-            os.remove("key")
+        # If key/contact exists but users does not, users.enc was likely deleted.
+        # -Delete data that is tied to previous user
+        if os.path.exists("key.enc"): os.remove("key.enc")
+        if os.path.exists("contacts.enc"): os.remove("contacts.enc")
+        if os.path.exists("salt.enc"): os.remove("salt.enc")
 
         print("No users are registered with this client.")
         response = input("Do you want to register a new user (y/n)?: ").lower()
@@ -88,40 +91,11 @@ class SecureDrop(cmd.Cmd):
                 print("Response not recognized.")
                 response = input("Please enter 'yes'(y) or 'no'(n): ")
 
-    def load_users(self):
-        """Load existing users from users.json"""
-        if os.path.exists("users.json"):
-            with open("users.json", "r") as file:
-                self.users = json.load(file)
-    
-    def load_contacts(self):
-        """Load existing contacts from contacts.json."""
-        if os.path.exists("contacts.json"):
-            with open("contacts.json", "r") as file:
-                self.contacts = json.load(file)
-
-    def save_users(self):
-        """Save users to the JSON file."""
-        with open("users.json", "w") as file:
-            json.dump(self.users, file)
-    
-    # Currently overwrites entire contacts.json file on every call.
-    # -Probably not a big deal, but theoretically inefficient for 
-    #   large numbers of contacts.
-    # -Could be optimized to only save changes.
-    def save_contacts(self):
-        """Save users to the JSON file."""
-        with open("contacts.json", "w") as file:
-            json.dump(self.contacts, file)
-
-    # TODO: Security for username?
     def register_user(self):
         """user registration."""
         full_name = input("Enter Full Name: ")
 
         # Email acquisition loop
-        # Figured it might be better to loop it instead of ending registration
-        # Maybe add a way to switch to login in case they forgot they registered?
         # NOTE: This is likely unnecessary as the app only supports one 
         #       registered user at a time.
         while True:
@@ -132,56 +106,106 @@ class SecureDrop(cmd.Cmd):
                 break
 
         # Password acquisition loop
-        # -Currently hashing after confirmation, but this does technically leave
-        #   the program more vulnerable to memory dumping in specific scenarios.
         while True:
-            password = input("Enter Password: ")
-            confirm_password = input("Re-enter Password: ")
-            if password != confirm_password:
-                print("Error: Passwords do not match!")
-            else:
+            password = getpass("Enter Password: ")
+            confirm_password = getpass("Re-enter Password: ")
+            if password == confirm_password:
                 print("Passwords Match.")
-                password = sign(password)
+                password = hash_b2b(password, generate_salt())
                 break
+            else:
+                print("Error: Passwords do not match!")
 
         self.users[email] = {
             "full_name": full_name,
             "password": password
         }
-        self.save_users()
+        encrypt_and_store(self.users, "users.enc")
         print("User registered successfully!")
+        print("SecureDrop will now exit, restart and login to enter the SecureDrop shell.")
 
-    # Potentially want to add max login attempts to protect against brute force?
+    # Salting implementation doesn't seem useful if there's only a single salt used for a single password
     def user_login(self):
         """Login user with email + password"""
-        while True:
+        attempts = 0
+        with open("salt.enc", "rb") as file:
+            salt = file.read()
+        while attempts < 5:
             email = input("Enter Email Address: ")
-            password = input("Enter Password: ")
-            password = sign(password)
+            if email == "exit": break
+            password = hash_b2b(getpass("Enter Password: "), salt)
             if email in self.users and compare_digest(password, self.users[email]["password"]):
                 return
             print("Email and Password Combination Invalid.\n")
+            attempts += 1
+        # Not using "do_exit" here as the return value could potentially be modified to bypass login
+        self.users = {}
+        if attempts >= 5:
+          print("Login failed: Maximum attempts reached")
+          time = datetime.datetime.now()
+          with open("report.log", "a") as file:
+              file.write(time.strftime("%c") + "\n")
+        print("Exiting SecureDrop")
+        sys.exit()
 
-# Hash password for security.
-# Currently using blake2b with a locally stored key
-# idk how tf to use these crypto libraries
-def sign(password):
-    blake2b = hashlib.blake2b(key=get_key(), digest_size=16)
-    blake2b.update(password.encode())
-    return blake2b.hexdigest()
+# ----- Hashing -----
+def generate_salt():
+    salt = get_random_bytes(SALT_SIZE)
+    with open("salt.enc", "wb") as file:
+        file.write(salt)
+    return salt
 
+def hash_b2b(data, salt):
+    """Returns blake2b hash of `data`"""
+    data = data.encode()
+    b2b = blake2b(key=get_key(), salt=salt)
+    b2b.update(data)
+    return b2b.hexdigest()
+
+# ----- Encryption -----
 # Can't help but feel like storing the key in its own file is counterintuitive
-# to security, should find a more secure solution.
-# -Embed it somewhere within the hash itself?
+# -Embed it somewhere? Feel like that wouldn't help much.
+# -Should probably use unique keys for individual files & hashing, instead of one for everything
 def get_key():
+    """Returns key stored in file, or generates one if it doesn't exist"""
     if os.path.exists("key"):
         with open("key", "rb") as file:
             key = file.read()
     else:
         with open("key", "wb") as file:
-            key = os.urandom(32)
+            key = get_random_bytes(KEY_SIZE)
             file.write(key)
     return key
+
+# AES-encryption: first 16-bytes are the nonce and last 16 bytes are the tag
+def encrypt_aes(data):
+    """Returns AES encrypted `data` as bytes"""
+    cipher = AES.new(get_key(), AES.MODE_GCM)
+    ciphertext, tag = cipher.encrypt_and_digest(data.encode())
+    ciphertext = cipher.nonce + ciphertext + tag
+    return ciphertext
+
+def decrypt_aes(data):
+    """Returns AES decrypted `data` as bytes"""
+    cipher = AES.new(get_key(), AES.MODE_GCM, nonce=data[:NONCE_SIZE])
+    return cipher.decrypt_and_verify(data[NONCE_SIZE:-TAG_SIZE], data[-TAG_SIZE:])
+
+# ----- JSON I/O -----
+# Can't serialize bytes to json, so encrypting entire object for storage
+# Downside: Data not encrypted while in memory
+def load_and_decrypt(source):
+    """Returns decrypted JSON object stored in `source`, if it exists."""
+    if os.path.exists(source):
+        with open(source, "rb") as file:
+            decrypted = decrypt_aes(file.read())
+            return json.loads(decrypted)
+    return {}
+
+def encrypt_and_store(obj, dest):
+    """Serializes & Encrypts JSON-compatible `obj` then writes it to `dest`."""
+    encrypted = encrypt_aes(json.dumps(obj))
+    with open(dest, "wb") as file:
+        file.write(encrypted)
 
 if __name__ == '__main__':
     SecureDrop().cmdloop()
